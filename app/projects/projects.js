c3edb6d697be8af10f9ed72935852da455911c56
liorhollinger <liorhollinger@gmail.com>
2016-06-09 19:09:31 +0300

    9.6.16

--- app/projects/projects.js
+++ app/projects/projects.js
@@ -1,20 +1,20 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute', 'ngAnimate'])
+angular.module('myApp.projects', ['ngRoute', 'ngAnimate'])
 
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view2', {
-            templateUrl: 'view2/view2.html',
-            controller: 'View2Ctrl as vm'
+        $routeProvider.when('/projects', {
+            templateUrl: 'projects/projects.html',
+            controller: 'projectsCtrl as vm'
         });
         $routeProvider.when('/project/:id', {
-            templateUrl: 'view2/proj.html',
+            templateUrl: 'projects/proj.html',
             controller: 'ProjCtrl as vm'
         });
 
     }])
 
-    .controller('View2Ctrl', ['DataFactory', function (DataFactory) {
+    .controller('projectsCtrl', ['DataFactory', function (DataFactory) {
         //console.log('DataFactory:', DataFactory);
         this.projs = DataFactory.getProjs();
     }])
